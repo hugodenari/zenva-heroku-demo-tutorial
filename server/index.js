d7af7b59f8cf9163f6997c6b795a937f17fb7384
hugodenari <48888211+hugodenari@users.noreply.github.com>
2019-03-24 17:24:04 -0300

    Add files via upload

---
+++ server/index.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const jsdom = require('jsdom');
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io').listen(server);
+const Datauri = require('datauri');
+
+const datauri = new Datauri();
+const { JSDOM } = jsdom;
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+function setupAuthoritativePhaser() {
+  JSDOM.fromFile(path.join(__dirname, 'authoritative_server/index.html'), {
+    // To run the scripts in the html file
+    runScripts: "dangerously",
+    // Also load supported external resources
+    resources: "usable",
+    // So requestAnimatinFrame events fire
+    pretendToBeVisual: true
+  }).then((dom) => {
+    dom.window.URL.createObjectURL = (blob) => {
+      if (blob){
+        return datauri.format(blob.type, blob[Object.getOwnPropertySymbols(blob)[0]]._buffer).content;
+      }
+    };
+    dom.window.URL.revokeObjectURL = (objectURL) => {};
+    dom.window.gameLoaded = () => {
+    let port = process.env.PORT;
+    if (port == null || port == "") {
+    port = 8082;
+      }
+    server.listen(port, function () {
+    console.log(`Listening on ${server.address().port}`);
+      });
+    };
+    dom.window.io = io;
+  }).catch((error) => {
+    console.log(error.message);
+  });
+}
+
+setupAuthoritativePhaser();
